919b6795cdf86e69dc7bd999a4905c1bf5c3d71e
Tang9511 <31895573+Tang9511@users.noreply.github.com>
2017-11-22 16:09:47 +0800

    0.0.1

--- first_py_project/test.py
+++ first_py_project/test.py
@@ -1,7 +1,10 @@
 import numpy as  np
 import pandas as pd
 
+
 #f=open('')
-data=pd.read_csv('/Users/tangshuyi/Downloads/textbooks & lecture notes/machine learning/ml_project/digits4000_trainset.csv')
-print(data.head)
+trainset=pd.read_csv('/Users/tangshuyi/Downloads/textbooks & lecture notes/machine learning/ml_project/digits4000_trainset.csv')
+digits_vec=pd.read_csv('/Users/tangshuyi/Downloads/textbooks & lecture notes/machine learning/ml_project/digits4000_digits_vec.csv')
+
+#print(data.head)
 print("hello,python")
