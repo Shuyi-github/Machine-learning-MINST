f3b85ce1ccd7483d22a80bba8f31ea48968e5e46
Tang9511 <31895573+Tang9511@users.noreply.github.com>
2017-11-22 12:43:34 +0800

    first commit

---
+++ first_py_project/test.py
@@ -0,0 +1,7 @@
+import numpy as  np
+import pandas as pd
+
+#f=open('')
+data=pd.read_csv('/Users/tangshuyi/Downloads/textbooks & lecture notes/machine learning/ml_project/digits4000_trainset.csv')
+print(data.head)
+print("hello,python")
